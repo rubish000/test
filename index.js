f200d834172992d702f219f28d0767816cc84a70
dipto-0008 <diptod906@gmail.com>
2025-03-26 00:58:09 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,33 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.get("/", (req,res)=>{
-  res.send("ok2")
-})
+
+app.get("/", (req, res) => {
+  res.send("ok22");
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/get-video', async (req, res) => {
   const videoUrl = 'https://www.youtube.com/watch?v=aAwlNGohFUs';
   const apiKey = 'rubish69';
-
   const apiUrl = `https://www.rubish.infy.uk/rubish/youtube-search?query=${encodeURIComponent(videoUrl)}&apikey=${apiKey}`;
 
   try {
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(apiUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+      }
+    });
+
     const url = response.data.videoInfo.medias[0].url;
-    const pp = await axios.get(url, { responseType: "arraybuffer" })
-    
-    const videoFilePath = path.join(__dirname, 'public', 'video.mp4'); 
-    fs.writeFileSync(videoFilePath, response.data)
-      const videoFileUrl = `https://ytb-apiz-69-seven.vercel.app/video.mp4`;
-      res.json({ videoFileUrl });
+    const pp = await axios.get(url, { responseType: "arraybuffer", headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' } });
+
+    const videoFilePath = path.join(__dirname, 'public', 'video.mp4');
+    fs.writeFileSync(videoFilePath, pp.data);
+
+    const videoFileUrl = `https://ytb-apiz-69-seven.vercel.app/video.mp4`;
+    res.json({ videoFileUrl });
   } catch (error) {
     console.error('Error fetching data from Rubish API:', error);
     res.status(500).send(error);
@@ -33,4 +40,3 @@ app.get('/get-video', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-        
